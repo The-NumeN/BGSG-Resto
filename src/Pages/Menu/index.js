c9e9b7c0db33d5fb5db3eb9e9d01f7cc25c0c4a7
Babacar <babacarg836@gmail.com>
2024-02-01 23:31:30 +0100

    pull

--- src/Pages/Menu/index.js
+++ src/Pages/Menu/index.js
@@ -3,19 +3,22 @@ import Card from "../../Components/Card";
 import './index.css';
 
 import blueImage from '../../Assets/cu2.png';
-import blackImage from '../../Assets/black.png';
-import pink from '../../Assets/pink.png'
+import blackImage from '../../Assets/cu3.png';
+import Image3 from '../../Assets/cu4.png';
+import Image4 from '../../Assets/fi1.png';
+import Image5 from '../../Assets/fi2.png';
+import Image6 from '../../Assets/fi3.png'
 import BackgroundImage from "../../Components/Footer/BackImage";
 import bg from "../../Assets/bg.jpg"
 
 export default function Menu() {
   const products = [
-    { img: blueImage, title: 'Test', description: "la bouffe n'est pas bonne", price: 20 },
-    { img: blackImage, title: 'Test2', description: "la bouffe n'est pas bonne", price: 30 },
-    { img: blueImage, title: 'Test3', description: "la bouffe n'est pas bonne", price: 50 },
-    { img: blueImage, title: 'Test3', description: "la bouffe n'est pas bonne", price: 60 },
-    { img: pink, title: 'Test3', description: "la bouffe n'est pas bonne", price: 70 },
-    { img: pink, title: 'Test3', description: "la bouffe n'est pas bonne", price: 80 },
+    { img: blueImage, title: 'Thiep', description: "Plat de riz avec des légumes frais", price: 20 },
+    { img: blackImage, title: 'Crevettes', description: "Crevettes fraichement préparées", price: 30 },
+    { img: Image3, title: 'Poulet', description: "Poulet avec sa sauce spéciale", price: 50 },
+    { img: Image4, title: 'Crustacés', description: "Ensemble de moules,coquillages...", price: 60 },
+    { img: Image5, title: 'Poisson', description: "Dorade", price: 70 },
+    { img: Image6, title: 'Poissonnet', description: "Thon", price: 80 },
   ];
 
   const addToCart = (product) => {
